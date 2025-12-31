df05e31a404f79acf5ec09d2d07a8bd97b16d640
Angad0202 <angad04042002@gmail.com>
2025-12-31 13:41:16 +0530

    Fix PWA build error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
   disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 const nextConfig = {
